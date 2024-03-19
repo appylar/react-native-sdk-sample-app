28de655ed18331acc73fd1b59b63b2a181140790
Rakesh Palotra <ninjacoderrakesh@gmail.com>
2024-03-19 15:59:24 +0530

    Updated App.tsx and new sdk zip

--- App.tsx
+++ App.tsx
@@ -1,118 +1,167 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Text,
+  Button,
+  Platform,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import  RNAppylar, {
+  RNAppylarAdType,
+  RNAppylarView
+} from 'react-native-appylar-sdk';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+export default function App() {
+  const [show, setShow] = useState(false);
+  const appylarViewRef = useRef(null);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    RNAppylar.eventEmitterAppylar.addListener('onInitialized', onInitialized);
+    RNAppylar.eventEmitterAppylar.addListener('onError', onError);
+    RNAppylar.eventEmitterAppylar.addListener('onBannerShown', onBannerShown);
+    RNAppylar.eventEmitterAppylar.addListener('onNoBanner', onNoBanner);
+    RNAppylar.eventEmitterAppylar.addListener(
+      'onInterstitialShown',
+      onInterstitialShown
+    );
+    RNAppylar.eventEmitterAppylar.addListener(
+      'onInterstitialClosed',
+      onInterstitialClosed
+    );
+    RNAppylar.eventEmitterAppylar.addListener(
+      'onNoInterstitial',
+      onNoInterstitial
+    );
+    initialize();
+    return () => {
+      RNAppylar.eventEmitterAppylar.removeAllListeners('onInitialized');
+      RNAppylar.eventEmitterAppylar.removeAllListeners('onError');
+      RNAppylar.eventEmitterAppylar.removeAllListeners('onBannerShown');
+      RNAppylar.eventEmitterAppylar.removeAllListeners('onNoBanner');
+      RNAppylar.eventEmitterAppylar.removeAllListeners('onInterstitialShown');
+      RNAppylar.eventEmitterAppylar.removeAllListeners('onInterstitialClosed');
+      RNAppylar.eventEmitterAppylar.removeAllListeners('onNoInterstitial');
+    };
+  }, []);
+
+  const initialize = async () => {
+    console.log('initialize111');
+    var adTypes = [RNAppylarAdType.banner, RNAppylarAdType.interstitial];
+    var appKey =
+      Platform.OS === 'android'
+        ? 'oq8KqmAv7CmWST23FS12-g'
+        : 'knZidxcpufCJLTfwJbbm4w';
+    await RNAppylar.initialize(appKey, adTypes, false);
+  };
+
+  const onInitialized = (eventObj: any) => {
+    console.log('App.tsx onInitialized', eventObj);
+  };
+
+  const onError = (eventObj: any) => {
+    console.log('App.tsx onError', eventObj);
+  };
+
+  const onBannerShown = (eventObj: any) => {
+    console.log('App.tsx onBannerShown', eventObj);
+  };
+
+  const onNoBanner = (eventObj: any) => {
+    console.log('App.tsx onNoBanner', eventObj);
+  };
+
+  const onInterstitialShown = (eventObj: any) => {
+    console.log('App.tsx onInterstitialShown', eventObj);
+  };
+
+  const onInterstitialClosed = (eventObj: any) => {
+    console.log('App.tsx onInterstitialClosed', eventObj);
+  };
+
+  const onNoInterstitial = (eventObj: any) => {
+    console.log('App.tsx onNoInterstitial', eventObj);
+  };
+
+  const showBanner = async () => {
+    setShow(true);
+  };
+
+  const hideBanner = async () => {
+    setShow(false);
+  };
+
+  const showInterstitial = async () => {
+    await RNAppylar.showAd();
+  };
+
+  // const setParameters = async () => {
+  //   let obj = {
+  //     banner_height: ['90'],
+  //     age_restriction: ['18'],
+  //   };
+  //   RNAppylar.setParameters(obj);
+  // };
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <View style={styles.containerButtons}>
+        <Text style={styles.heading}>Appylar Sample App</Text>
+        <Button
+          onPress={() => showBanner()}
+          title="SHOW BANNER"
+          color="#841584"
+        />
+        <View style={styles.emptyView} />
+        <Button
+          onPress={() => hideBanner()}
+          title="HIDE BANNER"
+          color="#841584"
+        />
+        <View style={styles.emptyView} />
+        <Button
+          onPress={() => showInterstitial()}
+          title="SHOW INTERSTITIAL"
+          color="#841584"
+        />
+        <View style={styles.emptyView} />
+      </View>
+      <View style={styles.containerBanner}>
+        <RNAppylarView
+          show={show}
+          placementId=""
+          style={styles.box}
+          ref={appylarViewRef}
+        />
+      </View>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  containerButtons: {
+    flex: 1,
+    justifyContent: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  emptyView: {
+    height: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  containerBanner: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  heading: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  box: {
+    marginVertical: 50
   },
 });
-
-export default App;
